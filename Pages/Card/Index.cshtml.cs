fd7a63d4f76f3803ddaf63c0bcbc2a39bc2f1696
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2025-07-09 14:49:04 +0200

    feat: Add filter and logging
    Added filter for cards
    Logging is now available through custom logger
    Static file added for available domains
    feat: Add filter and logging

--- Pages/Card/Index.cshtml.cs
+++ Pages/Card/Index.cshtml.cs
@@ -2,42 +2,77 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using DHCardHelper.Models;
 using System.Text.Json;
+using DHCardHelper.Services;
 
 namespace DHCardHelper.Pages.Card
 {
     public class IndexModel : PageModel
     {
         public List<CardDto> Cards { get; set; } = new List<CardDto>();
+        public List<CardDto> FilteredCards { get; set; } = new List<CardDto>();
+        public string[] _availableDomains { get; set; }
+
+        [BindProperty]
+        public string SelectedItem { get; set; }
+
+        private readonly IMyLogger _logger;
         private readonly IWebHostEnvironment _env;
-        public IndexModel(IWebHostEnvironment env)
+        public IndexModel(IWebHostEnvironment env, IMyLogger logger)
         {
             _env = env;
+            _logger = logger;
         }
         public async Task OnGet()
         {
-            //Cards = new List<CardDto>
-            //{
-            //    new CardDto{Domain = "Arcana", Feature="Some text here", Id = 1, Level = 1, RecallCost = 1, Title = "Arcana Card 1", Type = "Spell" },
-            //    new CardDto{Domain = "Arcana", Feature="Some text here 2", Id = 2, Level = 1, RecallCost = 1, Title = "Arcana Card 2", Type = "Ability" },
-            //    new CardDto{Domain = "Bone", Feature="Some text here 3", Id = 3, Level = 1, RecallCost = 1, Title = "Bone Card 1", Type = "Ability" },
-            //};
-            var arcanaCards = await ReadCardsFromJsonAsync("arcana.json");
-            var bladeCards = await ReadCardsFromJsonAsync("blade.json");
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            _availableDomains = await ReadAvailableDomains();
+
+            if (_availableDomains.Length == 0)
+                throw new ArgumentNullException("There are no available domains.");
 
             var IdCounter = 0;
-
-            foreach (var card in arcanaCards)
+            foreach (var domain in _availableDomains)
             {
-                card.Id = IdCounter;
-                Cards.Add(card);
-                IdCounter++;
+                var domainCards = await ReadCardsFromJsonAsync($"{domain}.json");
+                foreach (var card in domainCards)
+                {
+                    card.Id = IdCounter;
+                    Cards.Add(card);
+                    FilteredCards.Add(card);
+                    IdCounter++;
+                }
             }
+        }
+        public async Task<IActionResult> OnPostSelectedDomain()
+        {
+            await LoadData();
 
-            foreach (var card in bladeCards)
+            if (SelectedItem == "All")
+                FilteredCards = Cards;
+            else
+                FilteredCards = FilteredCards.Where(c => c.Domain == SelectedItem).ToList();
+
+            return Page();
+        }
+        private async Task<string[]> ReadAvailableDomains()
+        {
+            try
             {
-                card.Id = IdCounter;
-                Cards.Add(card);
-                IdCounter++;
+                var filePath = Path.Combine(_env.ContentRootPath, "Data", "AvailableDomains.txt");
+                var result = await System.IO.File.ReadAllLinesAsync(filePath);
+
+                _logger.Info("File loaded successfully!");
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message);
+                throw new Exception(ex.Message);
             }
         }
 
@@ -47,11 +82,12 @@ namespace DHCardHelper.Pages.Card
             {
                 var filePath = Path.Combine(_env.ContentRootPath, "Data", name.ToLower());
                 using var stream = System.IO.File.OpenRead(filePath);
+
                 return await JsonSerializer.DeserializeAsync<List<CardDto>>(stream);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                _logger.Error(ex.Message);
                 throw new Exception(ex.Message);
             }
         }
---
+++ Services/ConsoleLogger.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DHCardHelper.Services
+{
+    public class ConsoleLogger : IMyLogger
+    {
+        private ILogger<ConsoleLogger> _logger;
+        private readonly IWebHostEnvironment _env;
+        public ConsoleLogger(IWebHostEnvironment env, ILogger<ConsoleLogger> logger)
+        {
+            _env = env;
+            _logger = logger;
+        }
+        public void Info(string message)
+        {
+            _logger.LogInformation(Format("INFO", message));
+        }
+        public void Error(string message)
+        {
+            _logger.LogError(Format("ERROR", message));
+        }
+        private string Format(string type, string message)
+        {
+            return $"[{type}] [{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{message}]";
+        }
+    }
+}
