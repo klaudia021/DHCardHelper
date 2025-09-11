9317a6781f963db978fa10bd805704e4a51e6536
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2025-09-11 16:36:43 +0200

    test: add missing unit tests for BackgroundCard Add page
    Added OnPostAsync_SavingWasSuccessful_RedirectToAddPage and OnPostAsync_SavingWasNotSuccessfulAndThrowsException_LogsAndReturnsPage tests for coverage

--- DHCardHelper.Tests/DHCardHelper/Areas/GameMaster/Pages/Cards/Background/AddModelTests.cs
+++ DHCardHelper.Tests/DHCardHelper/Areas/GameMaster/Pages/Cards/Background/AddModelTests.cs
@@ -2,10 +2,14 @@
 using DHCardHelper.Areas.GameMaster.Pages.Cards.Background;
 using DHCardHelper.Data.Repository.IRepository;
 using DHCardHelper.Models.Entities;
+using DHCardHelper.Models.Entities.Cards;
 using DHCardHelper.Models.ViewModels;
 using DHCardHelper.Services;
 using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System.Linq.Expressions;
 
@@ -14,19 +18,27 @@ namespace DHCardHelper.Tests.DHCardHelper.Areas.GameMaster.Pages.Cards.Backgroun
     public class AddModelTests
     {
         private readonly Mock<IUnitOfWork> _mockUnitOfWork;
-        private readonly Mock<IRepository<BackgroundCardType>> _mockRepo;
+        private readonly Mock<IRepository<BackgroundCardType>> _mockBackgroundCardTypeRepo;
         private readonly Mock<IMyLogger> _mockLogger;
         private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<ICardRepository> _mockCardRepo;
         private readonly AddModel _addPageModel;
 
         public AddModelTests()
         {
             _mockUnitOfWork = new Mock<IUnitOfWork>();
-            _mockRepo = new Mock<IRepository<BackgroundCardType>>();
+            _mockBackgroundCardTypeRepo = new Mock<IRepository<BackgroundCardType>>();
             _mockLogger = new Mock<IMyLogger>();
             _mockMapper = new Mock<IMapper>();
+            _mockCardRepo = new Mock<ICardRepository>();
 
-            _mockUnitOfWork.Setup(u => u.BackgroundCardTypeRepository).Returns(_mockRepo.Object);
+            _mockUnitOfWork.Setup(u => u.BackgroundCardTypeRepository)
+                .Returns(_mockBackgroundCardTypeRepo.Object);
+
+            _mockCardRepo.Setup(c => c.AddAsync(new BackgroundCard()));
+
+            _mockUnitOfWork.Setup(u => u.CardRepository)
+                .Returns(_mockCardRepo.Object);
 
             _addPageModel = new AddModel(_mockUnitOfWork.Object, _mockLogger.Object, _mockMapper.Object)
             {
@@ -38,7 +50,7 @@ namespace DHCardHelper.Tests.DHCardHelper.Areas.GameMaster.Pages.Cards.Backgroun
         public async Task OnGetAsync_BackgroundCardExists_PopulatesTheDropDown()
         {
             // Arrange
-            _mockRepo.Setup(r => r.GetAllAsync())
+            _mockBackgroundCardTypeRepo.Setup(r => r.GetAllAsync())
                 .ReturnsAsync(new List<BackgroundCardType>
                 {
                     new BackgroundCardType { Id = 1, Name = "Background card 1" },
@@ -63,7 +75,7 @@ namespace DHCardHelper.Tests.DHCardHelper.Areas.GameMaster.Pages.Cards.Backgroun
         public async Task OnGetAsync_BackgroundCardDoesNotExist_DropDownWillBeEmpty()
         {
             // Arrange
-            _mockRepo.Setup(r => r.GetAllAsync())
+            _mockBackgroundCardTypeRepo.Setup(r => r.GetAllAsync())
                 .ReturnsAsync(new List<BackgroundCardType>());
 
             // Act
@@ -94,7 +106,7 @@ namespace DHCardHelper.Tests.DHCardHelper.Areas.GameMaster.Pages.Cards.Backgroun
         public async Task OnPostAsync_ForeignKeyIsNotValid_ReturnsPage()
         {
             // Arrange
-            _mockRepo.Setup(r => r.AnyAsync(It.IsAny<Expression<Func<BackgroundCardType, bool>>>()))
+            _mockBackgroundCardTypeRepo.Setup(r => r.AnyAsync(It.IsAny<Expression<Func<BackgroundCardType, bool>>>()))
                     .ReturnsAsync(false);
 
             // Act
@@ -102,6 +114,46 @@ namespace DHCardHelper.Tests.DHCardHelper.Areas.GameMaster.Pages.Cards.Backgroun
 
             // Assert
             Assert.False(_addPageModel.ModelState.IsValid);
+            Assert.IsType<PageResult>(result);
+        }
+
+        [Fact]
+        public async Task OnPostAsync_SavingWasSuccessful_RedirectToAddPage()
+        {
+            // Arrange
+            _mockBackgroundCardTypeRepo.Setup(r => r.AnyAsync(It.IsAny<Expression<Func<BackgroundCardType, bool>>>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _addPageModel.OnPostAsync();
+
+            // Assert
+            Assert.IsType<RedirectResult>(result);
+            Assert.Equal("./Add", ((RedirectResult)result).Url);
+        }
+
+        [Theory]
+        [InlineData(typeof(DbUpdateException))]
+        [InlineData(typeof(Exception))]
+        public async Task OnPostAsync_SavingWasNotSuccessfulAndThrowsException_LogsAndReturnsPage(Type exceptionType)
+        {
+            // Arrange
+            _mockBackgroundCardTypeRepo.Setup(r => r.AnyAsync(It.IsAny<Expression<Func<BackgroundCardType, bool>>>()))
+                .ReturnsAsync(true);
+
+            _mockUnitOfWork.Setup(u => u.SaveAsync())
+                .Throws((Exception)Activator.CreateInstance(exceptionType));
+
+            Mock<ITempDataDictionary> MockTempData = new Mock<ITempDataDictionary>();
+            _addPageModel.TempData = MockTempData.Object;
+
+            _mockLogger.Setup(l => l.Error("Log error"));
+
+            // Act
+            var result = await _addPageModel.OnPostAsync();
+
+            // Assert
+            _mockLogger.Verify(l => l.Error(It.IsAny<string>()), Times.Once);
             Assert.IsType<PageResult>(result);
         }
     }
