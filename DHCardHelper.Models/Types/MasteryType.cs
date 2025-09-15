1df31a21478d72f369113642a37e4632d60e1b1b
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2025-09-15 16:29:48 +0200

    feat: add mastery type enum to subclass card
    Created an enum for mastery type in sublass card

---
+++ DHCardHelper.Models/Types/MasteryType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHCardHelper.Models.Types
+{
+    public enum MasteryType
+    {
+        Foundation = 0,
+        Specialization = 1,
+        Mastery = 2
+    }
+}
