da1bdaa34a2c602ea505ce32c817b98d39befecf
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2025-09-15 16:39:56 +0200

    feat: add subclass pages
    Added "Edit" and "Add" pages in the GameMaster area
    In the Player area the subclass cards are now visible, and editable
    Subclass folder is now renamed to "Subclasses" to match other folder namings
    Updated the link in _layout

---
+++ DHCardHelper/Areas/GameMaster/Pages/Cards/Subclasses/Edit.cshtml.cs
@@ -0,0 +1,114 @@
+using DHCardHelper.Data.Repository.IRepository;
+using DHCardHelper.Models.DTOs;
+using DHCardHelper.Models.Entities.Cards;
+using DHCardHelper.Models.Types;
+using DHCardHelper.Models.ViewModels;
+using DHCardHelper.Services;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace DHCardHelper.Areas.GameMaster.Pages.Cards.Subclass
+{
+    [Area("GameMaster")]
+    public class EditModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMyLogger _logger;
+        private readonly IMapper _mapper;
+
+        public EditModel(IUnitOfWork unitOfWork, IMyLogger logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [BindProperty]
+        public UpsertSubclassViewModel SubclassViewModel { get; set; } = new UpsertSubclassViewModel();
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var entity = await _unitOfWork.CardRepository.GetFirstOrDefaultAsync<SubclassCard>(c => c.Id == id);
+            if (entity == null)
+                return NotFound();
+
+            await PopulateDropDowns();
+
+            SubclassViewModel.SubclassCardDto = _mapper.Map<SubclassCardDto>(entity);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            await PopulateDropDowns();
+
+            if (!ModelState.IsValid)
+                return Page();
+
+            var entity = await _unitOfWork.CardRepository.GetFirstOrDefaultAsync<SubclassCard>(c => c.Id == id);
+            if (entity == null)
+                return NotFound();
+
+            var characterClassForeignKeyIsValid = await this.IsForeignKeyValid(_unitOfWork.CardRepository, c => c.CharacterClassId == SubclassViewModel.SubclassCardDto.CharacterClassId);
+            if (!characterClassForeignKeyIsValid)
+            {
+                this.AddErrorToModel(() => SubclassViewModel.SubclassCardDto.CharacterClassId);
+
+                return Page();
+            }
+
+            _mapper.Map(SubclassViewModel.SubclassCardDto, entity);
+
+            try
+            {
+                await _unitOfWork.SaveAsync();
+                TempData["Success"] = "Subclass edited successfully";
+
+                return Redirect("/Player/Cards/Subclasses/Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.Error(ex.Message);
+                TempData["Error"] = "Unable to save data. Please check the data.";
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message);
+                TempData["Error"] = "There was a database error. Please try again.";
+            }
+
+            return Page();
+        }
+
+        private async Task PopulateDropDowns()
+        {
+            var availableClasses = await _unitOfWork.CharacterClassRepository.GetAllAsync();
+            SubclassViewModel.AvailableClasses = availableClasses.Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Name,
+                Selected = c.Id == SubclassViewModel.SubclassCardDto.CharacterClassId
+            });
+
+            SubclassViewModel.AvailableMasteryTypes =
+                Enum.GetValues(typeof(MasteryType))
+                    .Cast<MasteryType>()
+                    .Select(m => new SelectListItem
+                    {
+                        Value = m.ToString(),
+                        Text = m.ToString(),
+                        Selected = m == SubclassViewModel.SubclassCardDto.MasteryType
+                    });
+        }
+    }
+}
