1dfa7e5c7079b909d41c34d517b8039a5e95c2f1
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2025-09-22 13:07:01 +0200

    feat: add character sheet create and access page
    Added 'Add' page for adding a user character
    Created Index page for a user's characters.
    Admins and GameMasters can access this page via their GameMaster area Characters page
    Characters page is tied to roles, adapted _Layout according to these
    Added Characters/Character/{id} page for accessing one character

---
+++ DHCardHelper/Areas/Player/Pages/Characters/Add.cshtml.cs
@@ -0,0 +1,71 @@
+using DHCardHelper.Data.Repository.IRepository;
+using DHCardHelper.Models.DTOs.Character;
+using DHCardHelper.Models.Entities.Characters;
+using DHCardHelper.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace DHCardHelper.Areas.Player.Pages.Characters
+{
+    public class AddModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMyLogger _logger;
+
+        [BindProperty]
+        public CharacterSheetDto CharacterSheetDto { get; set; }
+        public AddModel(IUnitOfWork unitOfWork, IMyLogger logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId.IsNullOrEmpty())
+            {
+                TempData["Error"] = "User is not found. Please try to login again!";
+                return Page();
+            }
+
+            var entity = new CharacterSheet
+            {
+                Name = CharacterSheetDto.Name,
+                UserId = userId
+            };
+
+            await _unitOfWork.CharacterSheetRepository.AddAsync(entity);
+
+            try
+            {
+                await _unitOfWork.SaveAsync();
+                TempData["Success"] = "Character created successfully!";
+
+                return Redirect($"/Player/Characters/Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                TempData["Error"] = "Unable to save the data. Please check the data!";
+                _logger.Error(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "There was a database error. Please try again!";
+                _logger.Error(ex.Message);
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ DHCardHelper/Areas/Player/Pages/Characters/Character.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DHCardHelper.Areas.Player.Pages.Characters
+{
+    public class CharacterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
