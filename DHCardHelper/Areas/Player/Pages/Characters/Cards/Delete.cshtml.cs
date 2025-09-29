ad3cfbc9a034d031e320dd87cb2396ec307587c8
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2025-09-29 12:16:53 +0200

    feat: add delete card from character
    Added Delete endpoint for deleting card from characters
    Added remove-card-from-sheet.js for fetching the endpoint without reloading the page
    Fixed cardId not showing in Character's Card

---
+++ DHCardHelper/Areas/Player/Pages/Characters/Cards/Delete.cshtml.cs
@@ -0,0 +1,52 @@
+using DHCardHelper.Data.Repository.IRepository;
+using DHCardHelper.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace DHCardHelper.Areas.Player.Pages.Characters.Cards
+{
+    [ValidateAntiForgeryToken]
+    [Area("Player")]
+    public class DeleteModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMyLogger _logger;
+        public DeleteModel(IUnitOfWork unitOfWork, IMyLogger logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+        public async Task<IActionResult> OnDeleteAsync(int? characterSheetId, int? cardId)
+        {
+            if (characterSheetId == null)
+                return new JsonResult(new { success = false, message = "Character not found!" });
+
+            if (cardId == null)
+                return new JsonResult(new { success = false, message = "Card not found!" });
+
+            var cardSheetEntity = await _unitOfWork.CardSheetRepository.GetFirstOrDefaultAsync(s => s.CardId == cardId && s.CharacterSheetId == characterSheetId);
+            if (cardSheetEntity == null)
+                return new JsonResult(new { success = false, message = "Entity not found by these parameters!" });
+
+            _unitOfWork.CardSheetRepository.Remove(cardSheetEntity);
+
+            try
+            {
+                await _unitOfWork.SaveAsync();
+
+                return new JsonResult(new { success = true, message = "Card removed successfully!" });
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.Error(ex.Message);
+                return new JsonResult(new { success = false, message = "There was an error while saving!" });
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message);
+                return new JsonResult(new { success = false, message = "Something went wrong!" });
+            }
+        }
+    }
+}
---
+++ DHCardHelper/wwwroot/js/remove-card-from-sheet.js
@@ -0,0 +1,46 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('#remove-card-icon').forEach(btn => {
+        btn.addEventListener('click', async () => {
+            const cardId = btn.dataset.cardid;
+            const characterSheetId = btn.dataset.charactersheetid;
+            const token = document.querySelector('input[name="__RequestVerificationToken"]').value;
+
+            if (!cardId || cardId == 0 || !characterSheetId || characterSheetId == 0) {
+                toastr.error("Card or character not found!");
+
+                return;
+            }
+
+            const result = await Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#21cf35",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            });
+
+            if (result.isConfirmed) {
+                const response = await fetch(`/Characters/${characterSheetId}/Delete/${cardId}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Accept": "application/json",
+                        "RequestVerificationToken": token
+                    }
+                });
+
+                const data = await response.json();
+
+                if (data.success) {
+                    toastr.success(data.message);
+                    const card = document.getElementById(`card-${cardId}`)
+                    if (card)
+                        card.remove();
+                }
+                else
+                    toastr.error(data.message);
+            }
+        });
+    });
+});
